d1785ba9b776cad074dc9c86ac6ec24b22da2011
Recha5567 <alexandre.recha@gmail.com>
2026-04-23 15:11:20 +0100

    Update alunos.py

--- src/alunos.py
+++ src/alunos.py
@@ -10,43 +10,51 @@ def listar_alunos():
 
 
 def adicionar_aluno(nome, idade, telefone):
-    if not validar_nome(nome):    #AA
+    if not validar_nome(nome):
         return 400, "Nome invalido"
+
     if not idade.isdigit() or not (5 <= int(idade) <= 120):
         return 400, "Idade invalida"
+
     if not validar_telefone(telefone):
-        return 400, "Telefone invalido"    #aaa
-    aluno = {"nome": nome.title(), "idade": int(idade), "telefone": telefone}
+        return 400, "Telefone invalido"
+
+    aluno = {
+        "nome": nome.title(),
+        "idade": int(idade),
+        "telefone": telefone
+    }
     alunos.append(aluno)
     return 201, aluno
 
 
 def editar_aluno(indice, nome=None, idade=None, telefone=None):
     if not str(indice).isdigit() or not (0 <= int(indice) - 1 < len(alunos)):
-        return 404, "Aluno não encontrado.", []
+        return 404, "Aluno nao encontrado."
+
     a = alunos[int(indice) - 1]
-    erros = []
+
     if nome:
-        if validar_nome(nome):
-            a['nome'] = nome.title()
-        else:
-            erros.append("Nome inválido. Mantido anterior.")
+        if not validar_nome(nome):
+            return 400, "Nome invalido"
+        a['nome'] = nome.title()
+
     if idade:
-        if idade.isdigit() and 5 <= int(idade) <= 120:
-            a['idade'] = int(idade)
-        else:
-            erros.append("Idade inválida. Mantido anterior.")
-#a
+        if not idade.isdigit() or not (5 <= int(idade) <= 120):
+            return 400, "Idade invalida"
+        a['idade'] = int(idade)
+
     if telefone:
-        if validar_telefone(telefone):
-            a['telefone'] = telefone
-        else:
-            erros.append("Telefone inválido. Mantido anterior.")
-    return 200, a, erros
-#aaa
+        if not validar_telefone(telefone):
+            return 400, "Telefone invalido"
+        a['telefone'] = telefone
+
+    return 200, a
+
 
 def deletar_aluno(indice):
     if not str(indice).isdigit() or not (0 <= int(indice) - 1 < len(alunos)):
-        return 404, "Aluno não encontrado."
+        return 404, "Aluno nao encontrado."
+
     aluno = alunos.pop(int(indice) - 1)
     return 200, aluno
