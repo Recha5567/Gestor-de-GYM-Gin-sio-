772640999d43b8576af5cf3677fdedc68c1d439b
Recha5567 <alexandre.recha@gmail.com>
2026-05-13 09:58:05 +0100

    Update alunos.py

--- src/alunos.py
+++ src/alunos.py
@@ -1,7 +1,21 @@
+
+import json
+import os
 from utilities import validar_nome, validar_telefone, validar_idade
 
-alunos = []
+def carregar_dados(arquivo):
+    if os.path.exists(arquivo):
+        with open(arquivo, "r", encoding="utf-8") as f:
+            return json.load(f)
+    return []
 
+
+def guardar_dados(arquivo, dados):
+    with open(arquivo, "w", encoding="utf-8") as f:
+        json.dump(dados, f, ensure_ascii=False, indent=4)
+
+ARQUIVO = "alunos.json"
+alunos = carregar_dados(ARQUIVO)
 
 def listar():
     if not alunos:
@@ -16,6 +30,7 @@ def adicionar(nome, idade, telefone):
         return 400, "Idade inválida."
     if not validar_telefone(telefone):
         return 400, "Telefone inválido."
+    guardar_dados(ARQUIVO, alunos)
 
     aluno = {"nome": nome.title(), "idade": int(idade), "telefone": telefone}
     alunos.append(aluno)
@@ -25,6 +40,7 @@ def adicionar(nome, idade, telefone):
 def editar(indice, nome=None, idade=None, telefone=None):
     if not str(indice).isdigit() or not (0 <= int(indice) - 1 < len(alunos)):
         return 404, "Aluno não encontrado."
+    guardar_dados(ARQUIVO, alunos)
 
     a = alunos[int(indice) - 1]
 
@@ -42,6 +58,7 @@ def editar(indice, nome=None, idade=None, telefone=None):
     if telefone:
         a["telefone"] = telefone
 
+    guardar_dados(ARQUIVO, alunos)
     return 200, a
 
 
@@ -49,3 +66,4 @@ def deletar(indice):
     if not str(indice).isdigit() or not (0 <= int(indice) - 1 < len(alunos)):
         return 404, "Aluno não encontrado."
     return 200, alunos.pop(int(indice) - 1)
+guardar_dados(ARQUIVO, alunos)
