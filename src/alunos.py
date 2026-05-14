05eefa08cf12435c1a1c03f6aa78bf70e442bb53
Recha5567 <alexandre.recha@gmail.com>
2026-05-14 15:42:13 +0100

    Update alunos.py

--- src/alunos.py
+++ src/alunos.py
@@ -3,44 +3,49 @@ import json
 import os
 from utilities import validar_nome, validar_telefone, validar_idade
 
-def carregar_dados(arquivo):
-    if os.path.exists(arquivo):
-        with open(arquivo, "r", encoding="utf-8") as f:
+ARQUIVO = "alunos.json"
+
+def carregar_dados():
+    if os.path.exists(ARQUIVO):
+        with open(ARQUIVO, "r", encoding="utf-8") as f:
             return json.load(f)
     return []
 
 
-def guardar_dados(arquivo, dados):
-    with open(arquivo, "w", encoding="utf-8") as f:
+def guardar_dados(dados):
+    with open(ARQUIVO, "w", encoding="utf-8") as f:
         json.dump(dados, f, ensure_ascii=False, indent=4)
 
-ARQUIVO = "alunos.json"
-alunos = carregar_dados(ARQUIVO)
+
+
 
 def listar():
+    alunos = carregar_dados()
     if not alunos:
         return 204, "Nenhum aluno registado."
     return 200, alunos
 
 
 def adicionar(nome, idade, telefone):
+    alunos = carregar_dados()
     if not validar_nome(nome):
         return 400, "Nome inválido."
     if not validar_idade(idade):
         return 400, "Idade inválida."
     if not validar_telefone(telefone):
         return 400, "Telefone inválido."
-    guardar_dados(ARQUIVO, alunos)
+
 
     aluno = {"nome": nome.title(), "idade": int(idade), "telefone": telefone}
     alunos.append(aluno)
+    guardar_dados(alunos)
     return 201, aluno
 
 
 def editar(indice, nome=None, idade=None, telefone=None):
+    alunos = carregar_dados()
     if not str(indice).isdigit() or not (0 <= int(indice) - 1 < len(alunos)):
         return 404, "Aluno não encontrado."
-    guardar_dados(ARQUIVO, alunos)
 
     a = alunos[int(indice) - 1]
 
@@ -58,12 +63,15 @@ def editar(indice, nome=None, idade=None, telefone=None):
     if telefone:
         a["telefone"] = telefone
 
-    guardar_dados(ARQUIVO, alunos)
+    guardar_dados(alunos)
     return 200, a
 
 
 def deletar(indice):
+    alunos = carregar_dados()
     if not str(indice).isdigit() or not (0 <= int(indice) - 1 < len(alunos)):
         return 404, "Aluno não encontrado."
-    return 200, alunos.pop(int(indice) - 1)
-guardar_dados(ARQUIVO, alunos)
+    aluno_removido = alunos.pop(int(indice) - 1)
+    guardar_dados(alunos)
+    return 200, aluno_removido
+
